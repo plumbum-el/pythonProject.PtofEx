cba7f4828f12b3d3de4ce93f66d4be93386184fa
itclass <elson.e@bk.ru>
2024-01-26 13:40:49 +0300

    Половина решения

---
+++ task_1.py
@@ -0,0 +1,15 @@
+"""
+Решение задачи 1 предпрофессионального экзамена
+Программа читает данные из CSV-файла, ищет результат учащегося,
+Затем подсчитывает средний балл, заменяет им результаты None
+и сохраняет новый файл
+"""
+
+from csv import reader, writer
+
+with open("students.csv", encoding='utf-8') as data_file:
+    csv_data = reader(data_file, delimiter=',')
+    for row in csv_data:
+        if "Хадаров Владимир" in row[1]:
+            print(f"Ты получил: {row[4]}, за проект - {row[2]}")
+
